0096928bd920393cee1f9d56050bd89164794d54
kgbsibbi <kgbsibbi@gmail.com>
2017-03-31 12:43:31 +0900

    Simple-Nu-Observed-Value Error fix
    -3bytes 단위로 숫자를 처리하는데 1개의 바이트에서 음수 발생하여 잘못된 값 나오는 문제 수정
    - 체온계 메시지에 정밀도 너무 높게 나오는 문제 수정

--- ParseExcercise/src/com/kaispring/message/Message.java
+++ ParseExcercise/src/com/kaispring/message/Message.java
@@ -9,6 +9,7 @@ public class Message {
 	public static void main(String[] args){
 		int[] message1 = new int[27];// Blood Pressure Monitor
 		int[] message2 = new int[23]; // Thermometer
+		int[] message3 = new int[47]; // Weighing Scale
 		
 		message1[0] = 0xE200;
 		message1[1] = 0x0032;
@@ -46,13 +47,53 @@ public class Message {
 		message2[14] = 0x0010;
 		message2[15] = 0x0001;
 		message2[16] = 0x000C;
-		message2[17] = 0xFB38;message2[18] = 0x7520;
+		message2[17] = 0xFF00;message2[18] = 0x0172;
 		message2[19] = 0x2008;message2[20] = 0x0506;
 		message2[21] = 0x0830;message2[22] = 0x0000;
+		
+		// Weighing scale Data Reporting
+		message3[0] = 0xE700; 
+		message3[1] = 0x005A;
+		message3[2] = 0x0058; 
+		message3[3] = 0x1236;
+		message3[4] = 0x0101;
+		message3[5] = 0x0052;
+		message3[6] = 0x0000; 
+		message3[7] = 0x0000;message3[8] = 0x0000;
+		message3[9] = 0x0D1D; // event-type=MDC_NOTI_SCAN_REPORT_FIXED
+		message3[10] = 0x0048;
+		message3[11] = 0xF000;
+		message3[12] = 0x0000;
+		message3[13] = 0x0004; //ScanReportInfoFixed.obs-scan-fixed.count = 4
+		message3[14] = 0x0040;
+		message3[15] = 0x0001;
+		message3[16] = 0x000C;
+		message3[17] = 0xFF00;message3[18] = 0x02FA; // Simple-Nu-Observed-value = 76.2
+		message3[19] = 0x2007;message3[20] = 0x1206;
+		message3[21] = 0x1210;message3[22] = 0x0000;
+		
+		message3[23] = 0x0003;
+		message3[24] = 0x000C;
+		message3[25] = 0xFF00;message3[26] = 0x00F3; // Simple-Nu-Observed-Value = 24.3
+		message3[27] = 0x2007;message3[28] = 0x1206;
+		message3[29] = 0x1210;message3[30] = 0x0000;
+		
+		message3[31] = 0x0001;
+		message3[32] = 0x000C;
+		message3[33] = 0xFF00;message3[34] = 0x02F8; // Simple-Nu-Observed-value = 76.20
+		message3[35] = 0x2007;message3[36] = 0x1206;
+		message3[37] = 0x2005;message3[38] = 0x0000;
+		
+		message3[39] = 0x0003;
+		message3[40] = 0x000C;
+		message3[41] = 0xFF00;message3[42] = 0x00F2; // Simple-Nu-Observed-Value = 24.2
+		message3[43] = 0x2007;message3[44] = 0x1206;
+		message3[45] = 0x2005;message3[46] = 0x0000;
 		
 		
 		APDU apdu= new APDU();
 		apdu.parse(message2, 0);
+		//apdu.parse(message3, 0);
 		
 		XML xml = new XML(apdu);
 		Terminal terminal = new Terminal(apdu);
--- ParseExcercise/src/com/kaispring/structures/SimpleNuObservedValue.java
+++ ParseExcercise/src/com/kaispring/structures/SimpleNuObservedValue.java
@@ -14,14 +14,21 @@ public class SimpleNuObservedValue extends Node {
 		ByteBuffer b = ByteBuffer.allocate(4);
 		b.putInt(input);
 		byte[] result = b.array();
-		
+		int mantissa = 0;
+		// Java가 bit 각각을 signed로 변환하기 때문에
+		// 숫자 전체(3bit)로는 음수가 안나와도 하나의 bit로 치면 에서 음수가 생길 수 있음.
+		int[] values = { 65536, 256, 1 };
+		for(int i=1; i < 4; i++){
+			int number = result[i];
+			if(number < 0) number+=256;
+			mantissa += number*values[i-1];
+		}
 		int exponent = result[0];
-		int mantissa = result[1] << 16 | result[2] << 8 | result[3];
-		
 		value = mantissa * Math.pow(10, exponent);
-		
-		parsedString = infoString = String.valueOf(value);
-		//infoString += " ("+mantissa+"x10^"+exponent+")";
+		//출력 소숫점 자리수
+		String format = "%." + Math.abs(exponent) +"f";
+		parsedString = infoString = String.format(format, value);
+		infoString += " ("+mantissa+"x10^"+exponent+")";
 		return startIndex;
 	}
 
